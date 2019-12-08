fc34049145c326d36ce8773ff3515a269834f4b5
alonrbar <alon.r.bar@gmail.com>
2019-12-08 13:55:49 +0200

    single webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
-const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
+const mode = process.env.NODE_ENV;
+console.log(`Creating bundle. Mode: ${mode}.`);  // eslint-disable-line no-console
+
 module.exports = {
+    mode,
     entry: [path.resolve('./src/index.ts')],
+    devtool: 'sourcemap',
     output: {
         path: path.resolve('./dist'),
+        filename: 'react-tablize.js',
         library: 'react-tablize',
         libraryTarget: 'umd',
         umdNamedDefine: true,
-        globalObject: 'this'
+        globalObject: 'this',
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
     },
     externals: [nodeExternals()],
     module: {
         rules: [{ 
-            test: /(\.js|\.jsx|\.ts|\.tsx)$/, 
+            test: /(\.ts|\.tsx)$/, 
             use: 'babel-loader' 
         }]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        extensions: ['.ts', '.tsx'],
         modules: [path.resolve('./src'), 'node_modules']
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin()
-    ]
+    }
 };
