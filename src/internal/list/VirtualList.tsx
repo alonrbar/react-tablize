13661ddef0a7440078eea10dafdb8d80cb1c2668
alonrbar <alon.r.bar@gmail.com>
2019-12-16 15:01:26 +0200

    fix VirtualList scrollbar issue

--- src/internal/list/VirtualList.tsx
+++ src/internal/list/VirtualList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { CustomScrollbars } from '../CustomScrollbars';
-import { RenderWindowCellProps, VirtualWindow } from '../window';
+import { DomUtils } from '../utils';
+import { RenderWindowCellProps, VirtualWindow, WindowCalculator } from '../window';
 import { List, ListProps } from './List';
 
 export interface VirtualListProps extends ListProps {
@@ -13,22 +14,24 @@ export class VirtualList extends React.PureComponent<VirtualListProps> implement
         return this.props.layout === 'horizontal';
     }
 
-    private tableRef = React.createRef<VirtualWindow>();
+    private windowRef = React.createRef<VirtualWindow>();
+
+    private initialCalculator = new WindowCalculator();
 
     public refresh() {
-        if (this.tableRef.current) {
-            this.tableRef.current.clearCache();
+        if (this.windowRef.current) {
+            this.windowRef.current.clearCache();
         }
         this.forceUpdate();
     }
 
     public scrollTo(offset: number): void {
-        if (this.tableRef.current) {
+        if (this.windowRef.current) {
 
             const top = (this.isHorizontal ? undefined : offset);
             const left = (this.isHorizontal ? offset : undefined);
 
-            this.tableRef.current.scrollTo({
+            this.windowRef.current.scrollTo({
                 scrollTop: top,
                 rawScrollLeft: left,
                 normalizedScrollLeft: left
@@ -40,7 +43,7 @@ export class VirtualList extends React.PureComponent<VirtualListProps> implement
         return (
             <VirtualWindow
 
-                ref={this.tableRef}
+                ref={this.windowRef}
 
                 direction={this.props.dir}
                 scrollDirection={this.isHorizontal ? 'horizontal' : 'vertical'}
@@ -54,8 +57,8 @@ export class VirtualList extends React.PureComponent<VirtualListProps> implement
                 columnCount={this.isHorizontal ? this.props.itemCount : 1}
                 rowCount={this.isHorizontal ? 1 : this.props.itemCount}
 
-                columnWidth={this.isHorizontal ? this.props.itemSize : this.props.width}
-                rowHeight={this.isHorizontal ? this.props.height : this.props.itemSize}
+                columnWidth={this.isHorizontal ? this.props.itemSize : this.props.width - this.getVerticalScrollbarWidth()}
+                rowHeight={this.isHorizontal ? this.props.height - this.getHorizontalScrollbarWidth() : this.props.itemSize}
 
                 overscanColumnsCount={this.isHorizontal ? this.props.overscan : 0}
                 overscanRowCount={this.isHorizontal ? 0 : this.props.overscan}
@@ -79,6 +82,48 @@ export class VirtualList extends React.PureComponent<VirtualListProps> implement
     //
     // render helpers
     //
+
+    private getVerticalScrollbarWidth(): number {
+        const scrollableHeight = this.getScrollableHeight();
+        const hasVerticalScrollbar = scrollableHeight > this.props.height;
+        return (hasVerticalScrollbar && DomUtils.scrollbarWidth) || 0;
+    }
+
+    private getHorizontalScrollbarWidth(): number {
+        const scrollableWidth = this.getScrollableWidth();
+        const hasHorizontalScrollbar = scrollableWidth > this.props.width;
+        return (hasHorizontalScrollbar && DomUtils.scrollbarWidth) || 0;
+    }
+
+    private getScrollableHeight() {
+        if (this.windowRef.current)
+            return this.windowRef.current.getScrollableHeight();
+
+        // Body not rendered yet - have to calculate independently.  
+        // Not so DRY, breaks encapsulation and not very efficient either but I
+        // couldn't find a better way to do that which is not terribly ugly...
+        return this.initialCalculator.getTotalSize(
+            'row',
+            this.props.itemSize,
+            undefined,
+            this.props.itemCount
+        );
+    }
+
+    private getScrollableWidth() {
+        if (this.windowRef.current)
+            return this.windowRef.current.getScrollableWidth();
+
+        // Body not rendered yet - have to calculate independently.  
+        // Not so DRY, breaks encapsulation and not very efficient either but I
+        // couldn't find a better way to do that which is not terribly ugly...
+        return this.initialCalculator.getTotalSize(
+            'column',
+            this.props.itemSize,
+            undefined,
+            this.props.itemCount
+        );
+    }
 
     private getOuterElementType() {
         return this.props.customScrollbar ? CustomScrollbars : undefined;
